96dbd2612747e82e311f76126f61934a95f11dad
DioufFLR <geoffreyfleur@gmail.com>
2022-10-15 17:23:35 +0200

    Ajout de l'exercice 3

--- FRONT/assets/js/ex2.js
+++ FRONT/assets/js/ex2.js
@@ -3,4 +3,20 @@ let b = 100;
 let c = 1.00;
 let d = true;
 
-document.write("Ceci est une chaîne de caractère : " + a );
+
+// Concaténation d'une chaîne avec la valeur de a
+console.log("Ceci est une chaîne de caractère : " + a );
+
+// Décrémentation de b
+b--;
+console.log("La valeur de b est de : " + b);
+
+// Ajout de a à la valeur de c
+c += a;
+console.log("Voici la valeur de c après l'ajout de la valeur a : " + c);
+
+// Inverser la valeur de d
+if (d === true){
+    d = false;
+}
+console.log("Voici la valeur inversée de d : " + d);
---
+++ FRONT/assets/js/ex3.js
@@ -0,0 +1,3 @@
+// Exercice 1 Parité
+
+
