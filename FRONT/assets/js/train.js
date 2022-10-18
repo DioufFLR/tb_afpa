7aa1769c0609cf652651308d11ed0882e9ab51b2
Fleur geoffrey <fleur.geoffey@orange.fr>
2022-10-18 15:05:58 +0200

    Ajout du fichier train.js

---
+++ FRONT/assets/js/train.js
@@ -0,0 +1,38 @@
+// ----------------------Les boucles
+
+// Boucle while
+
+// let i = 0;
+// while (i < 10) {
+//     console.log("bonjour" + i);
+//     i = i + 1;
+// }
+
+// Boucle for itérer plusieurs fois
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("Bonjour");
+// }
+
+// Boucle for in itérer sur les clés d'un objet ou d'un tableau
+
+// const notes = [12, 13, 16, 8]
+// const person = {
+//     firstname: "John",
+//     lastname: "Doe"
+// }
+// for (let i in notes) {
+//     console.log(notes[i]);
+// }
+
+// const greeting = 'Bonjour';
+// for (let letter in greeting) {
+//     console.log(letter)
+// }
+
+// Boucle for of (parcourir les différents éléments d'un tableau
+
+// const greeting = 'Bonjour';
+// for (let letter of greeting) {
+//     console.log(letter)
+// }
