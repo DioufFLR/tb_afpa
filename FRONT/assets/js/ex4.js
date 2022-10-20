e5855a66ea31b28509a29d6900e3d6b98c4205eb
Fleur geoffrey <fleur.geoffey@orange.fr>
2022-10-20 15:24:34 +0200

    Ajout de l'exercice les boucles exercice 2

--- FRONT/assets/js/ex4.js
+++ FRONT/assets/js/ex4.js
@@ -1,17 +1,51 @@
-let i = 1;
-let firstname = String(prompt("Saisissez le prénom n " + i + " ou cliquez sur annuler pour arrêter la saisie"));
+// -----------------------Exercice 1 Saisie
 
-let prenom1 = document.createElement('p');
-prenom1.textContent = firstname;
-document.body.appendChild(prenom1);
+// let i = 1;
+// let firstname = String(prompt("Saisissez le prénom n " + i + " ou cliquez sur annuler pour arrêter la saisie"));
+//
+// let prenom1 = document.createElement('p');
+// prenom1.textContent = firstname;
+// document.body.appendChild(prenom1);
+//
+// let prenom2;
+//
+// while (firstname !== "") {
+//     i++;
+//     firstname = prompt("Saisissez le prénom n " + i + " ou cliquez sur annuler pour arrêter la saisie")
+//     prenom2 = document.createElement("p");
+//     prenom2.textContent = firstname;
+//     document.body.appendChild(prenom2);
+// }
+// console.log(i);
 
-let prenom2;
+// ----------------------Exercice 2 Entiers inférieurs à N
 
-while (firstname !== "") {
+// let i = 0;
+// let number = window.prompt("Saisissez un nombre");
+//
+// let result;
+// for (i = 0; i < number; i++) {
+//     result = i;
+//     let letter = document.createElement("p");
+//     letter.textContent = result;
+//     document.body.appendChild(letter);
+// }
+
+// ----------------------Exercice 3 Moyenne
+
+let i = 0;
+let note = parseInt(prompt("Saisissez une note et saisissez 0 si vous voulez calculez la moyenne de vos notes"));
+let moy;
+
+let result;
+while (note != 0) {
+    result = note + i;
+    console.log(note);
     i++;
-    firstname = prompt("Saisissez le prénom n " + i + " ou cliquez sur annuler pour arrêter la saisie")
-    prenom2 = document.createElement("p");
-    prenom2.textContent = firstname;
-    document.body.appendChild(prenom2);
+    note = prompt("Saisissez une note et saisissez 0 si vous voulez calculez la moyenne de vos notes");
+    if (note === 0) {
+        moy = result
+    }
 }
 
+console.log(i);
