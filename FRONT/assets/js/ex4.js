5d8074fe61d6cededd90e1620947c8ebe95df393
DioufFLR <geoffreyfleur@gmail.com>
2022-10-20 23:33:08 +0200

    ajout de l'exercice multiples

--- FRONT/assets/js/ex4.js
+++ FRONT/assets/js/ex4.js
@@ -33,12 +33,12 @@
 
 // ----------------------Exercice 3 Moyenne
 // let i;
-// let nbr1 = parseInt(prompt("Saisissez une note et saisissez 0 si vous voulez calculez la moyenne de vos notes"));
+// let nbr1 = parseInt(prompt("Saisissez une note et saisissez 0 si vous voulez calculer la moyenne de vos notes"));
 // let somme = nbr1;
 //
 // for (i = 0; nbr1 !== 0; i++) {
 //     console.log(nbr1);
-//     nbr1 = parseInt(prompt("Saisissez une note et saisissez 0 si vous voulez calculez la moyenne de vos notes"));
+//     nbr1 = parseInt(prompt("Saisissez une note et saisissez 0 si vous voulez calculer la moyenne de vos notes"));
 //     somme += nbr1;
 // }
 // console.log(somme);
@@ -46,4 +46,18 @@
 // alert("Votre moyenne est égal à : " + moyenne);
 
 // ----------------------Exercice 4 Multiples
+//
+// let i = 1;
+// let multiple = parseInt(prompt("Rentrez le multiple que vous souhaitez"));
+// let fois = parseInt(prompt("Combien de fois voulez vous le multipliez"));
+// let result;
+//
+// while (i !== fois) {
+//     i++;
+//     result = i * multiple;
+//     console.log(result);
+// }
+
+// ----------------------Exercice 5 Nombre de voyelles
+
 
