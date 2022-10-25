d2d11cc97c1ec2d51081aade0f41142ecd465924
Fleur geoffrey <fleur.geoffey@orange.fr>
2022-10-25 16:19:43 +0200

    Ajout de l'exercice les fonctions exercice 2

--- FRONT/assets/js/ex5.js
+++ FRONT/assets/js/ex5.js
@@ -1,34 +1,47 @@
 // ----------------------------------Exercice V Les fonctions
 
 // ----------------------Exercice 1
-let x = prompt("Saisissez un nombre");
-let y = prompt("Saisissez un nombre");
-let result;
-let result2;
+// let x = prompt("Saisissez un nombre");
+// let y = prompt("Saisissez un nombre");
+// let result;
+// let result2;
+//
+// let produit = function (x, y) {
+//     result = x * y;
+// }
+// produit(x, y);
+//
+// function cube(x) {
+//     result2 = x * x * x;
+// }
+// cube(x);
+//
+// // Intégration images avec js
+//
+// function i() {
+//     let img = document.createElement("img");
+//     img.src = "https://ncode.amorce.org/ressources/Pool/TB_DEV/WEB_Javascript_BASES/images/papillon.jpg";
+//     let div = document.getElementById("i");
+//     div.appendChild(img);
+// }
+// i();
+//
+// document.write("Le cube de " + x + " est égal à : " + result2);
+// document.write("<br>Le produit de " + x + " x " + y + "  est égal à : " + result);
 
-let produit = function (x, y) {
-    result = x * y;
+
+// --------------Exercice 2 String token
+
+let str1 = "robert ;dupont ;amiens ;8000";
+let str2 = ";";
+
+function strtrok(str1, str2, n) {
+    str2 = str1.split(str2);
+    document.getElementById("x").innerHTML = str2[n-1];
 }
-produit(x, y);
 
-function cube(x) {
-    result2 = x * x * x;
-}
-cube(x);
+let n = parseInt(prompt("Entrez le numéro du not voulant être obtenu"));
 
-// Intégration images avec js
+strtrok(str1, str2, n);
 
-function i() {
-    let img = document.createElement("img");
-    img.src = "https://ncode.amorce.org/ressources/Pool/TB_DEV/WEB_Javascript_BASES/images/papillon.jpg";
-    let div = document.getElementById("i");
-    div.appendChild(img);
-}
-i();
-
-document.write("Le cube de " + x + " est égal à : " + result2);
-document.write("<br>Le produit de " + x + " x " + y + "  est égal à : " + result);
-
-
-// --------------Exercice 2
 
