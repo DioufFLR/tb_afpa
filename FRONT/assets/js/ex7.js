77b281ea0bcf6002f747e0d69ca91108e5bd74b5
Fleur geoffrey <fleur.geoffey@orange.fr>
2022-10-27 15:47:52 +0200

    ex7.js ajout regex pour code postal

--- FRONT/assets/js/ex7.js
+++ FRONT/assets/js/ex7.js
@@ -1,7 +1,9 @@
-// Sélectionner le bon formulaire
+// ----------------------Sélectionner le bon formulaire------------------------------
+
 let form = document.getElementById("formTest");
 
-// Regex pour l'input societe
+// ----------------------Regex pour l'input societe-------------------------------
+
 form.societe.addEventListener("change", function () {
     validSociete(this);
 });
@@ -11,12 +13,13 @@ const validSociete = function (inputSociete) {
     // Test de la valeur de l'input de societe
     let testSociete = regexSociete.test(inputSociete.value);
     if (testSociete === false) {
-        alert("Veuillez rentrez au moins un caractère");
+        alert("Veuillez rentrez au moins un caractère et/ou uniquement des lettres");
     }
     console.log(testSociete);
 }
 
-// Regex pour l'input personnesContact
+// ----------------------Regex pour l'input personnesContact------------------------
+
 form.personnesContact.addEventListener("change", function () {
     validPersonnesContact(this);
 });
@@ -26,8 +29,38 @@ const validPersonnesContact = function (inputPersonnesContact) {
     // Test de la valeur de l'input de societe
     let testPersonnesContact = regexPersonnesContact.test(inputPersonnesContact.value);
     if (testPersonnesContact === false) {
-        alert("Veuillez rentrez au moins un caractère");
+        alert("Veuillez rentrez au moins un caractère et/ou uniquement des lettres");
     }
     console.log(testPersonnesContact);
 }
 
+// ----------------------Regex pour le code postal avec 5 caractères numériques------
+
+form.codePostal.addEventListener("change", function () {
+    validCodePostal(this);
+});
+
+const validCodePostal = function (inputCodePostal) {
+    let regexCodePostal = new RegExp(/^([0-9]{5})$/);
+    let testCodePostal = regexCodePostal.test(inputCodePostal.value);
+    if (testCodePostal === false) {
+        alert("Veuillez rentrez un code postal valide");
+    }
+    console.log(testCodePostal);
+}
+
+// ----------------------Regex pour l'input ville---------------------------------------
+
+form.ville.addEventListener("change", function () {
+    validVille(this);
+});
+
+const validVille = function (inputVille) {
+    let regexVille = new RegExp(/^[a-zA-Z]+$/);
+    // Test de la valeur de l'input de societe
+    let testVille = regexVille.test(inputVille.value);
+    if (testVille === false) {
+        alert("Veuillez rentrez au moins un caractère et/ou uniquement des lettres");
+    }
+    console.log(testVille);
+}
